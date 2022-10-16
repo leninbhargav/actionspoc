20fc9ce1e0ea90c6d556d832e0c872242448d5a0
Bhargav Pallam <Bhargav.Pallam@siriusxm.com>
2022-10-16 18:46:46 -0400

    Second commit

--- poc/apps/actions/src/app/app.module.ts
+++ poc/apps/actions/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { OneModule } from '@poc/one';
+import { ThirdModule } from '@poc/third';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, OneModule, ThirdModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- poc/jest.config.js
+++ poc/jest.config.js
@@ -1,3 +1,11 @@
 module.exports = {
-  projects: ['<rootDir>/apps/actions', '<rootDir>/apps/api'],
+  projects: [
+    '<rootDir>/apps/actions',
+    '<rootDir>/apps/api',
+    '<rootDir>/apps/second',
+    '<rootDir>/libs/one',
+    '<rootDir>/libs/third',
+    '<rootDir>/libs/one-api',
+    '<rootDir>/libs/two-api',
+  ],
 };
